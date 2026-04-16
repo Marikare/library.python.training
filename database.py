7047ec6d6ee1df03d1b34eacbe88783584e71c03
Marina <marinaiokoiama@hotmail.com>
2026-04-15 21:48:06 -0300

    Correct file

---
+++ database.py
@@ -0,0 +1,12 @@
+import sqlite3
+conn = sqlite3.connect("library.db")
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS authors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE,
+    birth_date TEXT NOT NULL,
+    nationality TEXT NOT NULL,
+    description TEXT)""")
+conn.commit()
+conn.close()
